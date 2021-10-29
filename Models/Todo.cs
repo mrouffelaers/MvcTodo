354204369577edc7f653fdfac91f6e60e78bd13c
mrouffelaers <mrouffelaer@ensc.fr>
2021-10-29 11:29:38 +0200

    ajout controller et modification de la page de présentation

---
+++ Models/Todo.cs
@@ -0,0 +1,9 @@
+namespace MvcTodo.Models
+{
+    public class Todo
+    {
+        public int Id {get; set;}
+        public string Task {get; set;}
+        public bool Completed {get; set;}
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace MvcTodo
 {
@@ -24,6 +25,9 @@ namespace MvcTodo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcTodoContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("MvcTodoContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
